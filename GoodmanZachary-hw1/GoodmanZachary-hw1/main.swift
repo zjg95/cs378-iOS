d7003172af7b7cd21b740e96209892d991da2b4a
Zachary Goodman <zjgoodman@utexas.edu>
2015-09-04 15:14:43 -0500

    first commit

---
+++ GoodmanZachary-hw1/GoodmanZachary-hw1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GoodmanZachary-hw1
+//
+//  Created by Zach Goodman on 9/4/15.
+//  Copyright (c) 2015 Zach Goodman. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
