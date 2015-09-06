2de1c275714c277817a6bad4c2d152393a40a695
Zachary Goodman <zjgoodman@utexas.edu>
2015-09-05 21:59:56 -0500

    added main

--- GoodmanZachary-hw1/GoodmanZachary-hw1/main.swift
+++ GoodmanZachary-hw1/GoodmanZachary-hw1/main.swift
@@ -38,7 +38,7 @@ class Automobile {
     // create
     // ------
     
-    func create (make : String, model : String, numberOfDoors : Int, speed : Int) -> Automobile {
+    class func create (make : String, model : String, numberOfDoors : Int, speed : Int) -> Automobile {
         return Automobile (make: make, model: model, numberOfDoors: numberOfDoors, speed: speed)
     }
     
@@ -99,12 +99,42 @@ class Automobile {
     }
 }
 
+// ------------------
+// randomValueBetween
+// ------------------
+
+func randomValueBetween (min:UInt32, max:UInt32) -> UInt32 {
+    var randomValue:UInt32 = min + arc4random_uniform(UInt32(max - min + 1))
+    return randomValue
+}
+
 // ----
 // main
 // ----
 
-func main(){
-    println("Hello world!")
+func main () {
+    // initialize automobiles
+    
+    var car1 : Automobile = Automobile.create("Maserati", model: "GranTurismo", numberOfDoors: 2, speed: 0)
+    var car2 : Automobile = Automobile.create("Honda", model: "Accord", numberOfDoors: 4, speed: 0)
+    var car3 : Automobile = Automobile.create("Tesla", model: "S", numberOfDoors: 2, speed: 0)
+    
+    // loop
+    
+    let min : UInt32 = 0
+    let max : UInt32 = 16
+    
+    for i in 0..<10 {
+        car1.setSpeed(Int(randomValueBetween(min, max)))
+        car2.setSpeed(Int(randomValueBetween(min, max)))
+        car3.setSpeed(Int(randomValueBetween(min, max)))
+    }
+    
+    // print states
+    
+    car1.description()
+    car2.description()
+    car3.description()
 }
 
 main()
