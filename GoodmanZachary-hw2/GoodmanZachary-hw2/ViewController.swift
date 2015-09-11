830ec61518d2d5f1ccafa10d6968835c756bc541
Zachary Goodman <zjgoodman@utexas.edu>
2015-09-11 17:26:48 -0500

    added button code. keyboard is dismissed with enter

--- GoodmanZachary-hw2/GoodmanZachary-hw2/ViewController.swift
+++ GoodmanZachary-hw2/GoodmanZachary-hw2/ViewController.swift
@@ -8,10 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var cityField: UITextField!
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func btnSaveClicked(sender: AnyObject) {
+        if nameField.text.isEmpty || cityField.text.isEmpty {
+            myLabel.text = "You must enter a value for *both* name and city!!"
+        }
+        else {
+            let value = nameField.text + " - " + cityField.text
+            myLabel.text = value
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        nameField.resignFirstResponder()
+        cityField.resignFirstResponder()
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        nameField.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +42,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
