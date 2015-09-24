fbd0852fb5fa161ebdab78ee914fd0a787d3ad07
Zachary Goodman <zjgoodman@utexas.edu>
2015-09-24 16:48:11 -0500

    added proj 3

---
+++ GoodmanZachary-hw3/GoodmanZachary-hw3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoodmanZachary-hw3
+//
+//  Created by Zach Goodman on 9/24/15.
+//  Copyright (c) 2015 Zach Goodman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
