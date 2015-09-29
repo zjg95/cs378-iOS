2d5902264010cd77778711d7520e94e942bf8cb9
Zachary Goodman <zjgoodman@utexas.edu>
2015-09-29 12:08:21 -0500

    created initial interface

---
+++ GoodmanZachary-hw4/GoodmanZachary-hw4Tests/GoodmanZachary_hw4Tests.swift
@@ -0,0 +1,36 @@
+//
+//  GoodmanZachary_hw4Tests.swift
+//  GoodmanZachary-hw4Tests
+//
+//  Created by Zach Goodman on 9/29/15.
+//  Copyright © 2015 Zach Goodman. All rights reserved.
+//
+
+import XCTest
+@testable import GoodmanZachary_hw4
+
+class GoodmanZachary_hw4Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
