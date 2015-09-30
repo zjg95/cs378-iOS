6fea80781fb19d6fb716969c3858c08fdf4b6aa0
Zachary Goodman <zjgoodman@utexas.edu>
2015-09-30 00:56:55 -0500

    updated addCandidate view. keyboard is now dismissed

---
+++ GoodmanZachary-hw4/GoodmanZachary-hw4/AddCandidateViewController.swift
@@ -0,0 +1,80 @@
+//
+//  AddCandidateViewController.swift
+//  GoodmanZachary-hw4
+//
+//  Created by Zach Goodman on 9/30/15.
+//  Copyright © 2015 Zach Goodman. All rights reserved.
+//
+
+import UIKit
+
+class AddCandidateViewController: UIViewController, UITextFieldDelegate {
+    
+    // ------------
+    // data outlets
+    // ------------
+
+    @IBOutlet weak var firstNameField: UITextField!
+    @IBOutlet weak var lastNameField: UITextField!
+    @IBOutlet weak var stateField: UITextField!
+    @IBOutlet weak var partySegment: UISegmentedControl!
+    @IBOutlet weak var saveLabel: UILabel!
+    
+    // -----------
+    // save button
+    // -----------
+    
+    @IBAction func saveButton(sender: AnyObject) {
+        dismissKeyboard()
+        saveLabel.text = "Candidate Saved!"
+    }
+    
+    // ---------------------
+    // textFeildShouldReturn
+    // ---------------------
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        firstNameField.resignFirstResponder()
+        lastNameField.resignFirstResponder()
+        stateField.resignFirstResponder()
+        return true
+    }
+    
+    // ---------------
+    // dismissKeyboard
+    // ---------------
+    
+    func dismissKeyboard(){
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
+        firstNameField.delegate = self
+        lastNameField.delegate  = self
+        stateField.delegate     = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
